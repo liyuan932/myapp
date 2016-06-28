6350f44a970d3d5abb4c2769a7d1bc8922628b37
wb-liyuan.j <wb-liyuan.j@alibaba-inc.com>
2016-06-28 20:58:30 +0800

    httpclient

--- myapp-biz/src/main/java/com/mycompany/myapp/service/common/ServicePreconditions.java
+++ myapp-biz/src/main/java/com/mycompany/myapp/service/common/ServicePreconditions.java
@@ -1,15 +1,8 @@
 package com.mycompany.myapp.service.common;
 
 import com.mycompany.myapp.enums.msg.BaseMsgEnum;
-import com.mycompany.myapp.enums.msg.CommonMsgEnum;
 
 public class ServicePreconditions {
-
-  public static void checkArgument(boolean expression) {
-    if (!expression) {
-      throw new ServiceException(CommonMsgEnum.FAIL_BIZ_PARAM_ERROR);
-    }
-  }
 
   public static void checkArgument(boolean expression, BaseMsgEnum msgEnum) {
     if (!expression) {
--- myapp-biz/src/test/java/com/mycompany/myapp/service/BaseTest.java
+++ myapp-biz/src/test/java/com/mycompany/myapp/service/BaseTest.java
@@ -1,9 +1,63 @@
 package com.mycompany.myapp.service;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 @ContextConfiguration("classpath:spring-bean.xml")
 public class BaseTest extends AbstractTestNGSpringContextTests {
+	public static void main(String[] args) throws IOException {
 
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpGet httpget = new HttpGet("http://www.biqugezw.com/3_3096/");
+
+			System.out.println("Executing request " + httpget.getRequestLine());
+
+			// Create a custom response handler
+			ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+				@Override
+				public String handleResponse(final HttpResponse response) throws IOException {
+					int status = response.getStatusLine().getStatusCode();
+					if (status >= 200 && status < 300) {
+						HttpEntity entity = response.getEntity();
+						return entity != null ? EntityUtils.toString(entity, "gb2312") : null;
+					} else {
+						throw new ClientProtocolException("Unexpected response status: " + status);
+					}
+				}
+
+			};
+			String responseBody = httpclient.execute(httpget, responseHandler);
+			System.out.println("----------------------------------------");
+			System.out.println(responseBody);
+
+			Matcher requstMatcher = Pattern.compile("<dd><a href=\"(.*)\">(.*)</a></dd>").matcher(responseBody);
+			while (requstMatcher.find()) {
+				System.out.println(requstMatcher.group(1));
+				System.out.println(requstMatcher.group(2));
+			}
+
+			FileWriter writer = new FileWriter("c:/a.txt");
+			writer.write(responseBody);
+			writer.flush();
+			writer.close();
+
+		} finally {
+			httpclient.close();
+		}
+	}
 }
