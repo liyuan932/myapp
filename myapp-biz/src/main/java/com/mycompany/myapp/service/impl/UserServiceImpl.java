7c8a835aec724c3c991ae2c27be7aa75fd259525
wb-liyuan.j <wb-liyuan.j@alibaba-inc.com>
2016-08-08 09:46:24 +0800

    add cache

--- myapp-biz/src/main/java/com/mycompany/myapp/service/impl/UserServiceImpl.java
+++ myapp-biz/src/main/java/com/mycompany/myapp/service/impl/UserServiceImpl.java
@@ -1,13 +1,5 @@
 package com.mycompany.myapp.service.impl;
 
-import java.util.List;
-
-import javax.annotation.Resource;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.mycompany.myapp.dao.UserDAO;
 import com.mycompany.myapp.daoobject.User;
 import com.mycompany.myapp.enums.category.EnableOrDisableEnum;
@@ -24,6 +16,14 @@ import com.mycompany.myapp.utils.DateUtil;
 import com.mycompany.myapp.utils.log.LogBean;
 import com.mycompany.myapp.utils.log.LogUtils;
 import com.mycompany.myapp.vo.UserVO;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Transactional
 @Service("userService")
@@ -34,7 +34,7 @@ public class UserServiceImpl extends BaseService implements UserService {
 	private UserDAO userDAO;
 
 	@Override
-	public List<UserVO> queryUser(UserQuery query) {
+	public List<UserVO> queryList(UserQuery query) {
 
 		List<User> users = userDAO.queryList(query);
 
@@ -49,17 +49,31 @@ public class UserServiceImpl extends BaseService implements UserService {
 	}
 
 	@Override
-	public User getUserById(Long id) {
+	@Cacheable(value="userCache")
+	public User getById(Long id) {
+		System.out.println("real querying account... {}");
+
 		return userDAO.getById(id);
 	}
 
 	@Override
-	public void addUser(User user) {
+	public boolean add(User user) {
 		LogBean logBean = LogUtils.newLogBean(MainFunctionEnum.USER_ADMIN, UserFunctionEnum.ADD_USER);
 		logBean.addParameters("user", user);
 
 		userDAO.insert(user);
 		ServicePreconditions.checkArgument(user.getId() != null, UserMsgEnum.FAIL_BIZ_ADD_USER);
+
+		return true;
+	}
+
+	@Override
+	@CacheEvict(value="userCache",key="#user.getId()")
+	public boolean update(User user) {
+
+		userDAO.update(user);
+
+		return true;
 	}
 
 	@Override
---
+++ myapp-test/src/test/java/com/mycompany/myapp/service/UserServiceTest.java
@@ -0,0 +1,21 @@
+package com.mycompany.myapp.service;
+
+import com.mycompany.myapp.daoobject.User;
+import org.springframework.test.annotation.Rollback;
+import org.testng.annotations.Test;
+
+import javax.annotation.Resource;
+
+public class UserServiceTest extends BaseTest{
+
+  @Resource
+  UserService userService;
+
+  @Test
+  @Rollback(false)
+  public void getUserById() {
+    User user1 = userService.getById(284L);
+    userService.update(user1);
+    User user2 = userService.getById(284L);
+  }
+}
